b4b77ff5db3eca5a5697c1a64fdd7a6501d427dd
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-14 16:37:59 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function receivesAFunction(spy){
+    return spy()
+  }
+  
+  function returnsANamedFunction(){
+    return function test(){
+    }
+  }
+  
+  function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+  }
